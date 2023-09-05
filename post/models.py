9d6498b23b3b804741c5f3a09e639617553eff3b
mohammadgoodarzi <m.goodarzi606@gmail.com>
2023-09-05 16:51:09 +0330

    create model post in app post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from core.models import *
+from accounts.models import User
 
-# Create your models here.
+
+class PostModel(CreateModel, UpdateModel, DeleteModel):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='post_users')
+    body = models.TextField(help_text='Please write caption')
+    image = models.ImageField(upload_to='posts',blank=True, null=True, help_text='Please upload your image')
+    video = models.FileField(upload_to='post/video', blank=True, null=True, help_text='please upload your video')
+    location = models.CharField(max_length=730, blank=True, null=True,
+                                help_text='You can write the location of this post')
+    slug = models.SlugField()
+    class StatusPost(models.Model):
+        class StatusPosts(models.TextChoices):
+            Published = 'pb', 'published'
+            Rejected = 'rg', 'rejected'
+
+        choose_status = models.CharField(
+            max_length=2,
+            choices=StatusPosts.choices,
+            default=StatusPosts.Published
+        )
+
+

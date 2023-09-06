07f0618bbdd3f8a801ac36ef95a30738c6800cac
mohammadgoodarzi <m.goodarzi606@gmail.com>
2023-09-07 00:15:34 +0330

    user can delete post and test deleted posts

--- post/urls.py
+++ post/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'post'
 urlpatterns = [
     path("", views.HomeView.as_view(), name='home'),
+    path('details/<int:post_id>/<str:post_slug>/', views.PostDetailsView.as_view(), name='post_details'),
+    path('details/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='delete_post')
     
 ]
--- post/views.py
+++ post/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import PostModel
+from accounts.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 class HomeView(View):
-    form_class = ''
     template_name = 'post/home.html'
     
-    def get(self, requet):
+    def get(self, request):
         post = PostModel.objects.all()
-        return render(requet, self.template_name, {'post': post})
+        return render(request, self.template_name, {'post': post})
     
 
+class PostDetailsView(LoginRequiredMixin, View):
+    template_name = 'post/post_details.html'
+    
+    def get(self, request, *args, **kwargs):
+        post = PostModel.objects.get(pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        # user = User.objects.all(user=post)
+        context = {
+            'post': post,
+            # 'user': user,
+            
+            
+        }
+        return render(request, self.template_name, context)
+    
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = PostModel.objects.get(pk=post_id)
+        context = {
+            'post': post
+        }
+        if request.user.id == post.user.id:
+            post.delete()
+            messages.success(request, 'Delete post', 'success')
+            return redirect('post:home')
+        else:
+            messages.error(request, 'Dont delete post', 'warning')
+            
+        return render(request, '', context)

38495ebe391405254e111ef72f2c79f14ef024fe
mohammadgoodarzi <m.goodarzi606@gmail.com>
2023-09-11 13:07:17 +0330

    create comment and test comment

--- post/forms.py
+++ post/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import PostModel
+from .models import CommentModel, PostModel
 
 
 class PostCreateUpdateForm(forms.ModelForm):
@@ -12,4 +12,13 @@ class PostCreateUpdateForm(forms.ModelForm):
 #     class Meta:
 #         model = PostModel
 #         fields = ('body', )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CommentModel
+        fields = ('body', )
         
+        widgets = {
+            'body': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'type comment'}),
+        }
--- post/views.py
+++ post/views.py
@@ -6,7 +6,7 @@ from .models import PostModel, CommentModel
 from accounts.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentForm
 from django.utils.text import slugify
 
 
@@ -20,23 +20,35 @@ class HomeView(View):
     
 
 class PostDetailsView(LoginRequiredMixin, View):
+
     
     def setup(self, request, *args, **kwargs):
-        self.post = get_object_or_404(PostModel, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        self.post_instance = get_object_or_404(PostModel, pk=kwargs['post_id'], slug=kwargs['post_slug'])
         return super().setup(request, *args, **kwargs)
     
     template_name = 'post/post_details.html'
     
+    form_class = CommentForm
     def get(self, request, *args, **kwargs):
-        post = self.post
+        post = self.post_instance
+        comment_form = self.form_class()
         comment = CommentModel.objects.filter(is_reply=True)
         context = {
             'post': post,
             'comment': comment,
-            
-            }
+            'comment_form': comment_form,}
         return render(request, self.template_name, context)
     
+    def post(self, request, *args, **kwargs):
+        comment_form = self.form_class(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'Comment created', 'success')
+            return redirect('post:post_details', self.post_instance.id, self.post_instance.slug)
+        return render(request, self.template_name)
 
 class PostDeleteView(LoginRequiredMixin, View):
     

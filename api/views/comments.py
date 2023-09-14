f3f8a1d4b912eb4367eb4b23fabfc54dfd2b7d30
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-09-14 12:18:03 +0330

    create comments with api

--- api/views/comments.py
+++ api/views/comments.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.serializers.commets import CommentsCreateSerializer
+from rest_framework import status
 
 
 class CommentsCreateApiView(APIView):
     def post(self, request):
-        ...
+        ser_data = CommentsCreateSerializer(data=request.data)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_201_CREATED)
         
         
         
